79de39f409c786d61cbf30788b1873e7c6eeab8f
Chintanrajsinh Gohil <gohilchintanrajsinh8169@gmail.com>
2022-01-16 13:46:57 +0530

    changing code structure

--- server/server.js
+++ server/server.js
@@ -2,10 +2,7 @@ const path = require("path");
 const express = require("express");
 const nodemailer = require("nodemailer");
 const validator = require("validator");
-
-
-
-
+const pug = require("pug");
 
 const app = express();
 app.use(express.json());
@@ -17,7 +14,6 @@ app.use(express.static(staticPath));
 app.set("view engine", "pug");
 app.set("views", viewsPath);
 
-
 app.get("/", (req, res) => {
 	res.render("index");
 });
@@ -28,7 +24,7 @@ app.post("/send", (req, res) => {
 	let toValid = true;
 	if (isMultiple) {
 		const toArr = req.body.to.split(",");
-		toArr.forEach(element => {
+		toArr.forEach((element) => {
 			if (!validator.isEmail(element)) {
 				toValid = false;
 			}
@@ -36,45 +32,47 @@ app.post("/send", (req, res) => {
 		if (!toValid) {
 			res.render("index", { err: `Enter Fields values properly !!` });
 		}
-	}
-	else {
+	} else {
 		toValid = validator.isEmail(to);
 	}
-	const isValid = validator.isEmail(req.body.from) && toValid && !validator.isEmpty(req.body.subject) && !validator.isEmpty(req.body.message.trim()) && !validator.isEmpty(req.body.password.trim());
+	const isValid =
+		validator.isEmail(req.body.from) &&
+		toValid &&
+		!validator.isEmpty(req.body.subject) &&
+		!validator.isEmpty(req.body.message.trim()) &&
+		!validator.isEmpty(req.body.password.trim());
 	if (isValid) {
 		const transporter = nodemailer.createTransport({
-			service: 'gmail',
+			service: "gmail",
 			auth: {
 				user: req.body.from,
-				pass: req.body.password.trim()
-			}
+				pass: req.body.password.trim(),
+			},
 		});
 
 		const mailOptions = {
 			from: req.body.from,
 			to: to.trim(),
 			subject: req.body.subject.trim(),
-			html: req.body.message.trim()
+			html: req.body.message.trim(),
 		};
 
 		transporter.sendMail(mailOptions, (error, info) => {
 			if (error) {
 				if (error.toString().includes("getaddrinfo ENOTFOUND")) {
 					res.render("index", { err: `Can't connect to network !! Turn On Internet !!` });
-				}
-				else if (error.response.includes("Application-specific password required")) {
+				} else if (error.response.includes("Application-specific password required")) {
 					const url = "https://myaccount.google.com/signinoptions/two-step-verification";
 					res.render("index", { err: `Disable two factor authentication from <a href=${url} target="_blank">Here</a> to send email !!` });
-				}
-				else if (error.response.includes("Username and Password not accepted")) {
+				} else if (error.response.includes("Username and Password not accepted")) {
 					const url = "https://myaccount.google.com/lesssecureapps";
 					res.render("index", {
 						err: `Your credential are not matching !!<br>
-						You should also check your gmail access for our app <a href=${url} target="_blank">Here</a> !!`
+						You should also check your gmail access for our app <a href=${url} target="_blank">Here</a> !!`,
 					});
-				}
-				else {
+				} else {
 					res.render("index", { err: error.response });
+					console.log(error);
 				}
 			} else {
 				if (info.response.includes("OK")) {
@@ -82,8 +80,7 @@ app.post("/send", (req, res) => {
 				}
 			}
 		});
-	}
-	else {
+	} else {
 		res.render("index", { err: `Enter Fields values properly !!` });
 	}
 });
